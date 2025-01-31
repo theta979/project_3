c4bf3dbaadea2128c0394e52d708cd8c62e465f7
Caleb Williams <calebhwilliams@gmail.com>
2025-01-31 17:27:11 +0000

    Update PhillyFoodDeserts.js
    Updated the .js file to include a functioning legend with color-coded key to low access status of the corresponding Census Tracts.

--- static/js/PhillyFoodDeserts.js
+++ static/js/PhillyFoodDeserts.js
@@ -5,7 +5,7 @@ let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
 // Create map of Philadelphia with layers
 let PhillyMap = L.map("map", {
-    center: [39.95, -75.16],
+    center: [39.99, -75.16],
     zoom: 12
 });
 
@@ -19,7 +19,8 @@ d3.json(link).then(geojsonData => {
     
     // Load the CSV data - Note: I had to run a server in the project folder in order to view the CSV data
     // In a terminal window run: python -m http.server 8000
-    d3.csv("static/data/Philadelphia_Food_Access.csv").then(csvData => {
+    // d3.csv("static/data/Philadelphia_Food_Access.csv").then(csvData => {
+    d3.csv("static/data/Philadelphia_Food_Access.csv").then(csvData => { 
 
         // Create a lookup for faster access
         let censusLookup = {};
@@ -86,14 +87,16 @@ function getColor(lowAccess5, lowAccess1) {
     } else {
         return "gray";
     }
-
-
-};
+}
+console.log(getColor(0, 0)); // Should be "green"
+console.log(getColor(1, 0)); // Should be "yellow"
+console.log(getColor(0, 1)); // Should be "red"
+console.log(getColor(-1, -1)); // Should be "gray"
 
 // Create a legend control object
-// let legend = L.control({
-//     position: "bottomright"
-// });
+let legend = L.control({
+    position: "bottomright"
+});
 
 // Then add all the details for the legend
 legend.onAdd = function () {
@@ -101,30 +104,41 @@ legend.onAdd = function () {
 
     // Set legend styles
     div.style.background = "white";
+    div.style.opacity = ".90";
     div.style.padding = "10px";
+    // div.style.border = "1px solid black";
+    div.style.borderRadius = "5px";
+    // div.style.width = "auto";
+    // div.style.display = "flex";
+    // div.style.flexDirection = "column";
     div.style.alignItems = "center";
 
+
     // Add title
-    div.innerHTML = "<h4 style='text-align: center; margin-bottom: 5px;'>Legend</h4>";
+    div.innerHTML = "<h2 style='text-align: center; margin-bottom: 10px;'>Legend</h2>";
 
     // Define categories based on getColor scheme
     let categories = [
         {label: "Not Low Access", color: getColor(0, 0)}, // Green
-        {label: "Low Access (> .5 miles)", color: getColor(1, 0)}, // Yellow
-        {label: "low Access (> 1 mile)", color: getColor(1, 1)}, // Red
+        {label: "Live > .5 miles from Quality food", color: getColor(1, 0)}, // Yellow
+        {label: "Live > 1 mile from Quality food", color: getColor(1, 1)}, // Red
         {label: "Undefined / Other", color: getColor(-1, -1)} // Gray (Default)
     ];
 
 
     // Loop through categories to create legend items
     categories.forEach(item => {
-        div.innerHTML += `<div style="display: flex; align-items: center; gap: 5px; margin-bottom: 5px;">
-            <i style="background" ${item.color}; width: 15px; height: 15px; display: inline-block;"></1>
+        div.innerHTML += `<div style="display: flex; align-items: center; gap: 5px; margin-bottom: 10px;">
+            <i style="background: ${item.color}; width: 18px; height: 18px; display: inline-block;"></i>
             ${item.label}
         </div>`;
     });
 
+    // Footnote below the Legend
+    div.innerHTML += `<p style="font-size: 12px; color: gray; margin-top: 10px;">
+        * As determined by USDA, Treasury, & HHS.
+    </p>`;
     return div;
 };
 // Finally, add the legend to the map.
-  legend.addTo(PhillyMap);
+legend.addTo(PhillyMap);
